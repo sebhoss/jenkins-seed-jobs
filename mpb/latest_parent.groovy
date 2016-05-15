9c47d310de9efb28a36ed783c69a633ca6e3bb34
Sebastian Hoß <mail@shoss.de>
2016-05-15 22:20:54 +0200

    filter projects earlier
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

--- mpb/latest_parent.groovy
+++ mpb/latest_parent.groovy
@@ -5,35 +5,33 @@ def jobBasePath = "mpb"
 def projectCatalog = new File("/var/git/stable/jenkins-jobs-setup/mpb/projects.json")
 def slurper = new JsonSlurper()
 def jsonText = projectCatalog.getText()
-def json = slurper.parseText(jsonText)
+def json = slurper.parseText(jsonText).findAll { !"maven-build-process".equals(it.name) }
 
 json.each {
     def project = it
-    if (!"maven-build-process".equals(project.name)) {
-        job("$jobBasePath/${project.name}_with_latest_parent") {
-            scm {
-                git(project.repository)
+    job("$jobBasePath/${project.name}_with_latest_parent") {
+        scm {
+            git(project.repository)
+        }
+        triggers {
+            project.upstreams.each {
+                upstream("$jobBasePath/${it}_deploy_to_local-nexus", "SUCCESS")
             }
-            triggers {
-                project.upstreams.each {
-                    upstream("$jobBasePath/${it}_deploy_to_local-nexus", "SUCCESS")
-                }
+        }
+        steps {
+            maven {
+                goals("versions:update-parent")
+                properties("generateBackupPoms": false)
+                mavenInstallation("maven-3.3.9")
+                providedGlobalSettings("talk-to-docker-nexus")
             }
-            steps {
-                maven {
-                    goals("versions:update-parent")
-                    properties("generateBackupPoms": false)
-                    mavenInstallation("maven-3.3.9")
-                    providedGlobalSettings("talk-to-docker-nexus")
-                }
-            }
-            steps {
-                maven {
-                    goals("clean")
-                    goals("verify")
-                    mavenInstallation("maven-3.3.9")
-                    providedGlobalSettings("talk-to-docker-nexus")
-                }
+        }
+        steps {
+            maven {
+                goals("clean")
+                goals("verify")
+                mavenInstallation("maven-3.3.9")
+                providedGlobalSettings("talk-to-docker-nexus")
             }
         }
     }
@@ -59,7 +57,6 @@ listView("$jobBasePath/Latest Parent") {
 }
 
 listView("$jobBasePath/Failure") {
-    description('All failing jobs of stable modules')
     jobs {
         json.each {
             name("${it.name}_with_latest_parent")
@@ -83,7 +80,6 @@ listView("$jobBasePath/Failure") {
 }
 
 listView("$jobBasePath/Success") {
-    description('All successful jobs of stable modules')
     jobs {
         json.each {
             name("${it.name}_with_latest_parent")
