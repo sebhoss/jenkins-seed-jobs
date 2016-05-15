c9e0bc5331324f672d59ac7d9e653df61f225bc2
Sebastian Hoß <mail@shoss.de>
2016-05-15 17:33:02 +0200

    add prototypes
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ prototype_modules.groovy
@@ -0,0 +1,85 @@
+import groovy.io.FileType
+
+String jobBasePath = 'prototype'
+
+folder(jobBasePath) {
+    description 'Contains prototype modules'
+}
+
+def jobNames = []
+def gitBasePath = "/var/git/prototypes"
+def base = new File(gitBasePath)
+base.eachDir() { directory ->
+    def path = directory.path
+    def pom = new File(path + "/pom.xml")
+    if (pom.exists()) {
+        def project = path.substring(gitBasePath.length() + 1).replaceAll('/','-')
+        def jobName = "$jobBasePath/$project"
+        jobNames << jobName
+        job(jobName) {
+            scm {
+                git(path)
+            }
+            steps {
+                maven {
+                    goals("clean")
+                    goals("deploy")
+                    mavenInstallation("maven-3.3.9")
+                    providedGlobalSettings("talk-to-docker-nexus")
+                }
+            }
+            steps {
+                maven {
+                    goals("sonar:sonar")
+                    properties("sonar.host.url": "http://sonar:9000")
+                    mavenInstallation("maven-3.3.9")
+                    providedGlobalSettings("talk-to-docker-nexus")
+                }
+            }
+        }
+    }
+}
+
+listView("$jobBasePath/Failure") {
+    description('All failing jobs of stable modules')
+    jobs {
+        names(jobNames as String[])
+    }
+    jobFilters {
+        status {
+            status(Status.UNSTABLE, Status.FAILED, Status.ABORTED)
+        }
+    }
+    recurse(true)
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
+
+listView("$jobBasePath/Success") {
+    description('All successful jobs of stable modules')
+    jobs {
+        names(jobNames as String[])
+    }
+    jobFilters {
+        status {
+            status(Status.STABLE)
+        }
+    }
+    recurse(true)
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
+}
