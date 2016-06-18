3c9ffa81a4d6e51c744fda58204111a5bef82d8c
Sebastian Hoß <mail@shoss.de>
2016-06-18 18:25:46 +0200

    define upstream projects
    Signed-off-by: Sebastian Hoß <mail@shoss.de>

---
+++ upstreams.groovy
@@ -0,0 +1,52 @@
+import groovy.json.JsonSlurper
+
+def projectCatalog = new File("/var/git/stable/jenkins-jobs-setup/projects.json")
+def slurper = new JsonSlurper()
+def jsonText = projectCatalog.getText()
+def json = slurper.parseText(jsonText)
+
+json.each {
+    def project = it
+    folder(project.name)
+    project.upstreams.each {
+        def upstreamProject = it
+        job("${project.name}/${project.name}_with_latest_${upstreamProject.name}") {
+            blockOnUpstreamProjects()
+            logRotator {
+                numToKeep(5)
+                daysToKeep(7)
+            }
+            scm {
+                git(project.repository)
+            }
+            triggers {
+                upstream("${upstreamProject.name}/${upstreamProject.name}_deploy_to_local-nexus", "SUCCESS")
+                cron("@daily")
+            }
+            steps {
+                maven {
+                    goals("versions:use-latest-versions")
+                    properties("includes": upstreamProject.include)
+                    properties("generateBackupPoms": false)
+                    properties("allowSnapshots": true)
+                    mavenInstallation("maven-latest")
+                    providedGlobalSettings("talk-to-local-nexus")
+                }
+            }
+            steps {
+                maven {
+                    goals("clean")
+                    goals("verify")
+                    mavenInstallation("maven-latest")
+                    providedGlobalSettings("talk-to-local-nexus")
+                }
+            }
+            publishers {
+                irc {
+                    strategy("ALL")
+                    notificationMessage("SummaryOnly")
+                }
+            }
+        }
+    }
+}
